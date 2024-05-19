095c0111271f5f09d357aa51c0b0ed8609096302
IsraelJorge <israeljorge2000@gmail.com>
2024-05-19 20:17:42 -0300

    fix: date undefined

--- src/utils/getFirstAndLastDate.ts
+++ src/utils/getFirstAndLastDate.ts
@@ -3,6 +3,8 @@ import { AccountReceivable } from '@/data/schemas/AccountReceivable'
 import { orderAccountReceivableByDate } from './orderAccountReceivableByDate'
 
 export const getFirstAndLastDate = (data: AccountReceivable[]) => {
+  if (!data.length) return { firstDate: new Date(), lastDate: new Date() }
+
   const sortedData = orderAccountReceivableByDate(data)
   const firstDate = sortedData[0].issuanceDate
   const lastDate = sortedData[sortedData.length - 1].issuanceDate
